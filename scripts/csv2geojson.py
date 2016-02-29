1335f9def46c93f74da350c5ddf48db617a2df60
DachiCoding <dachi.xu@gmail.com>
2016-02-29 09:20:17 -0600

    update border line

--- scripts/csv2geojson.py
+++ scripts/csv2geojson.py
@@ -1,5 +1,41 @@
 import csv
+import json
 
-path = "../data/datause1.csv"
-with open(path,'rb') as csvfile:
+csvpath = "./data/datause1.csv"
+jsonpath = "./data/datause1.json"
+
+jsonData=[]
+headers=[
+ "censusTract",
+ "Change_Population",
+ "Change_HouseUnits",
+ "Change_5To7_or_more_person_household",
+ "Change_Builst1939earlier",
+ "Change_Built_1940to1959",
+ "Change_VacantUnits",
+ "Y00_PCT_Units_HHIncome_25to50k",
+ "Y00_PCT_Units_HHIncome_50to75k",
+ "Chg_Educate_HighSchool_orLess",
+ "Chg_Educate_SomeCollege",
+]
+
+with open(csvpath,'rb') as csvfile:
   reader = csv.reader(csvfile, delimiter=',')
+  for row in reader:
+    tempRow = []
+    tractId = row[2]
+    tractId = tractId[9:20]
+    if tractId.isdigit():
+      tempRow.append(tractId)
+      for x in range(4,14):
+        tempRow.append(row[x])
+
+      tempJson = {}
+      for x in range(len(tempRow)):
+        tempJson[headers[x]] = tempRow[x]
+      json.dumps(tempJson)
+      jsonData.append(tempJson)
+
+with open(jsonpath, 'w') as jsonfile:
+  json.dump(jsonData,jsonfile)
+
